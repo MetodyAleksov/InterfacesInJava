2c8ac8394b8ba5a595c28ddf957c4e50c36ab20f
Metody Aleksov <teddy57603@gmail.com>
2022-06-13 19:03:48 +0300

    updated TaxReport class

--- src/com/company/TaxReport.java
+++ src/com/company/TaxReport.java
@@ -1,10 +1,24 @@
 package com.company;
 
 public class TaxReport {
+
     private ITaxCalculator taxCalculator;
 
+    //dependency injection through the constructor
     public TaxReport(ITaxCalculator taxCalculator) {
         this.taxCalculator = taxCalculator;
+    }
+
+    //dependency injection through a setter
+    public void setTaxCalculator(ITaxCalculator taxCalculator) {
+        this.taxCalculator = taxCalculator;
+    }
+
+    //Dependency injection through a method
+    public void chooseTaxCalculator(CalculatorType type){
+        if(type == CalculatorType.Default){
+            taxCalculator = new TaxCalculator(10000);
+        }
     }
 
     public String taxReport(){
